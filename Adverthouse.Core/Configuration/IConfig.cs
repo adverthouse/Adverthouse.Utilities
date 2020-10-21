a3db918b27cd6755b0d6c064065bd22c5a1d0dcc
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2020-10-21 16:02:52 +0300

    Core add

---
+++ Adverthouse.Core/Configuration/IConfig.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adverthouse.Core.Configuration
+{
+    public interface IConfig
+    {
+    }
+}
---
+++ Adverthouse.Core/Configuration/RedisConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adverthouse.Core.Configuration
+{
+    public class RedisConfig : IConfig
+    {
+        public bool Enabled { get; set; } = false;
+
+        public string RedisHost { get; set; } = "127.0.0.1";
+
+        public int RedisPort { get; set; } = 6379;
+ 
+    }
+}
