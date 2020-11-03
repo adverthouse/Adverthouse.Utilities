868a1334fedcfc4980144f19c2880213e0240f1e
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2020-11-03 12:37:55 +0300

    Naming changes

--- Adverthouse.Core/Configuration/SMTPConfig.cs
+++ Adverthouse.Core/Configuration/SMTPConfig.cs
@@ -8,12 +8,17 @@ namespace Adverthouse.Core.Configuration
     public class SMTPConfig : IConfig
     {
         public bool IsAuthenticationEnabled { get; set; } = false;
-        public SmtpDeliveryMethod deliverymethod { get; set; } = SmtpDeliveryMethod.Network;
+        public SmtpDeliveryMethod Deliverymethod { get; set; } = SmtpDeliveryMethod.Network;
         public bool EnableSSL { get; set; } = false;
         public string Encoding { get; set; } = "utf-8";        
         public int SMTPPort { get; set; } = 587;
-        public string SMTPserver { get; set; } = "";
+        public string SMTPServer { get; set; } = "";
         public string User { get; set; } = "";
-        public string Password { get; set; } = "";
+        public string Password { get; set; } = ""; 
+        public string FromAddress { get; set; } = "";
+        public string FromName { get; set; } = "";
+        public string TestReceiver { get; set; } = "";
+        public bool IsBCCEnabled { get; set; } = false;
+        public string BCCAddresses { get; set; } = "";  
     }
 }
