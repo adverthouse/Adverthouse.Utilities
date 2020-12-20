3aac63e272c7160f9b3cfafa062e51edb444e22b
Yunus ÖZTÜRK <yunusozturk@msn.com>
2020-12-20 13:43:42 +0300

    Due 6000 request limit ServiceStack removed.

--- Adverthouse.Common/Data/Redis/RedisCacheManager.cs
+++ Adverthouse.Common/Data/Redis/RedisCacheManager.cs
@@ -1,57 +1,54 @@
 ﻿using Adverthouse.Common.Interfaces;
 using Adverthouse.Common.NoSQL;
 using Adverthouse.Core.Configuration;
-using ServiceStack.Redis;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
+using RedisKey = Adverthouse.Common.NoSQL.RedisKey;
 
 namespace Adverthouse.Common.Data.Redis
 {
     public class RedisCacheManager : IRedisCacheManager
     {
-        private readonly RedisEndpoint _redisEndpoint;
-        private readonly IRedisClient _rc;
+        private ConnectionMultiplexer _connectionMultiplexer;
+        private IDatabase _database;
+        private int _currentDatabaseID = 0;
         private Core.Configuration.RedisConfig _redisConfig;
         public RedisCacheManager(AppSettings appSettings)
         {
-            _redisConfig = appSettings.RedisConfig;
-          
-            var host = _redisConfig.RedisHost;
-            var port = Convert.ToInt32(_redisConfig.RedisPort);
-            _redisEndpoint = new RedisEndpoint()
-            {
-                Host = host,
-                Port = port,
-                ConnectTimeout = 0
-            };
-            _rc = new RedisClient(_redisEndpoint);
+            _redisConfig = appSettings.RedisConfig; 
+            _connectionMultiplexer = ConnectionMultiplexer.Connect($"{ _redisConfig.RedisHost}:{ _redisConfig.RedisPort}");
+            _database = _connectionMultiplexer.GetDatabase(_currentDatabaseID);
         }
+
         public bool RemoveKey(RedisKey key) {
-            return _rc.Remove(key.Key);
+            return _database.KeyDelete(key.Key);
         }
         public bool IsKeyExist(RedisKey key)
         {
-            return _rc.ContainsKey(key.Key) ? true : false;
+            return _database.KeyExists(key.Key);
         }
 
         public bool NearToExpire(RedisKey key, TimeSpan time)
         {
-            TimeSpan? ttl = _rc.GetTimeToLive(key.Key);
+            TimeSpan? ttl = _database.KeyTimeToLive(key.Key);
             return ttl.HasValue ? (ttl.Value.Seconds < time.Seconds ? true : false) : false;
         }
     
         public void SetValue<T>(RedisKey key, T value, TimeSpan timeout)
         {
-            _rc.Set(key.Key, value, timeout);
+            string jsonData = JsonConvert.SerializeObject(value);
+            _database.StringSet(key.Key, jsonData, timeout); 
         }
 
         public T Get<T>(RedisKey key,Func<T> acquire)
         {
             if (IsKeyExist(key))
             {
-                var resultExist = _rc.Get<T>(key.Key);
+                var resultExist = _database.StringGet(key.Key);
 
                 if (key.ReCacheNearToExpires)
                 {
@@ -60,8 +57,8 @@ namespace Adverthouse.Common.Data.Redis
                         Task.Run(() => SetValue(key, acquire(), key.CacheTime));
                     }
                 }
- 
-                return resultExist;
+
+                return JsonConvert.DeserializeObject<T>(resultExist);
             }
 
             var result = acquire();
