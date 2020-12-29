abb62800fd61ea4e4e4f7912bb1dd53eef09402d
Yunus ÖZTÜRK <yunusozturk@msn.com>
2020-12-29 21:07:43 +0300

    connectionTimeout added to dapper.

--- Adverthouse.Common/Data/DapperSql.cs
+++ Adverthouse.Common/Data/DapperSql.cs
@@ -14,56 +14,58 @@ namespace Adverthouse.Common.Data
     public class DapperSql:IDapperSql
     {
         private string _connectionString = "";
+        private int _defaultConnectionTimeOut;
 
-        public DapperSql(string connectionString)
+        public DapperSql(string connectionString, int connectionTimeOut = 300)
         {
             _connectionString = connectionString;
+            _defaultConnectionTimeOut = connectionTimeOut;
         }
 
         public string GetConnectionString() {
             return _connectionString;
         }
-        public IEnumerable<T> Query<T>(string sqlCommand)
+        public IEnumerable<T> Query<T>(string sqlCommand, int? commandTimeout)
         {
             using (IDbConnection db2 = SqlConnection())
             { 
-                return db2.Query<T>(sqlCommand);
+                return db2.Query<T>(sqlCommand,commandTimeout: commandTimeout ?? _defaultConnectionTimeOut);
             }
         }
-        public IEnumerable<T> Query<T>(string sqlCommand, object param = null)
+        public IEnumerable<T> Query<T>(string sqlCommand, object param = null, int? commandTimeout = null)
         {
             using (IDbConnection db2 = SqlConnection())
             {
-                return db2.Query<T>(sqlCommand,param);
+                return db2.Query<T>(sqlCommand,param, commandTimeout: commandTimeout ?? _defaultConnectionTimeOut);
             }
         }
-        public int Execute(string sqlCommand)
+        public int Execute(string sqlCommand, int? commandTimeout)
         {
             using (IDbConnection db2 = SqlConnection())
             {
-                return db2.Execute(sqlCommand);
+                return db2.Execute(sqlCommand, commandTimeout: commandTimeout ?? _defaultConnectionTimeOut);
             }
         }
-        public int Execute(string sqlCommand, object param = null)
+        public int Execute(string sqlCommand, object param = null, int? commandTimeout =null)
         {
             using (IDbConnection db2 = SqlConnection())
             {
-                return db2.Execute(sqlCommand, param);
+                return db2.Execute(sqlCommand, param, commandTimeout: commandTimeout ?? _defaultConnectionTimeOut);
             }
         }
-        public T ExecuteScalar<T>(string sqlCommand)
+        public T ExecuteScalar<T>(string sqlCommand, int? commandTimeout)
         {
             using (IDbConnection db2 = SqlConnection())
             {
-                return db2.ExecuteScalar<T>(sqlCommand);
+                return db2.ExecuteScalar<T>(sqlCommand, commandTimeout: commandTimeout ?? _defaultConnectionTimeOut);
             }
         }
 
-        public T ExecuteScalar<T>(string sqlCommand, object param = null)
+        public T ExecuteScalar<T>(string sqlCommand, object param = null, int? commandTimeout = null)
         {
             using (IDbConnection db2 = SqlConnection())
             {
-                return db2.ExecuteScalar<T>(sqlCommand, param);
+                return db2.ExecuteScalar<T>(sqlCommand, param, commandTimeout: commandTimeout ?? _defaultConnectionTimeOut);
             }
         }
         public void Dispose() {}
--- Adverthouse.Common/Interfaces/IDapperSql.cs
+++ Adverthouse.Common/Interfaces/IDapperSql.cs
@@ -8,12 +8,12 @@ namespace Adverthouse.Common.Interfaces
     public interface IDapperSql : IDisposable
     {
         string GetConnectionString();
-        IEnumerable<T> Query<T>(string sqlCommand);
-        IEnumerable<T> Query<T>(string sqlCommand,object param = null);
-        int Execute(string sqlCommand);
-        int Execute(string sqlCommand, object param = null);
-        T ExecuteScalar<T>(string sqlCommand);
-        T ExecuteScalar<T>(string sqlCommand, object param = null);
+        IEnumerable<T> Query<T>(string sqlCommand, int? commandTimeout);
+        IEnumerable<T> Query<T>(string sqlCommand, object param = null, int? commandTimeout = null);
+        int Execute(string sqlCommand, int? commandTimeout);
+        int Execute(string sqlCommand, object param = null, int? commandTimeout = null);
+        T ExecuteScalar<T>(string sqlCommand, int? commandTimeout);
+        T ExecuteScalar<T>(string sqlCommand, object param = null, int? commandTimeout = null);
         SqlConnection SqlConnection();
     }
 }
