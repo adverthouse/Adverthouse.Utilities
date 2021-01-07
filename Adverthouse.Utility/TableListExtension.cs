5c708532c418e87d6c02c4b10f3402da5eaddb77
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2021-01-07 09:29:15 +0300

    TableListExtension

---
+++ Adverthouse.Utility/TableListExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adverthouse.Utility
+{
+    public static class TableListExtension
+    {
+        public static DataTable ListToDataTable<T>(IList<T> lst)
+        { 
+            DataTable currentDT = CreateTable<T>();
+
+            Type entType = typeof(T);
+
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entType);
+            foreach (T item in lst)
+            {
+                DataRow row = currentDT.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                {
+
+                    if (prop.PropertyType == typeof(Nullable<decimal>) || prop.PropertyType == typeof(Nullable<int>) || prop.PropertyType == typeof(Nullable<Int64>))
+                    {
+                        if (prop.GetValue(item) == null)
+                            row[prop.Name] = 0;
+                        else
+                            row[prop.Name] = prop.GetValue(item);
+                    }
+                    else
+                        row[prop.Name] = prop.GetValue(item);
+
+                }
+                currentDT.Rows.Add(row);
+            }
+
+            return currentDT;
+        }
+
+        public static DataTable CreateTable<T>()
+        {
+            Type entType = typeof(T);
+            DataTable tbl = new DataTable();
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entType);
+            foreach (PropertyDescriptor prop in properties)
+            {
+                if (prop.PropertyType == typeof(Nullable<decimal>))
+                    tbl.Columns.Add(prop.Name, typeof(decimal));
+                else if (prop.PropertyType == typeof(Nullable<int>))
+                    tbl.Columns.Add(prop.Name, typeof(int));
+                else if (prop.PropertyType == typeof(Nullable<Int64>))
+                    tbl.Columns.Add(prop.Name, typeof(Int64));
+                else
+                    tbl.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            return tbl;
+        }
+    }
+}
