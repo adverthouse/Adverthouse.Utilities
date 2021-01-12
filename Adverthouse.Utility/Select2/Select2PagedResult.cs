1ac0f762456a0ae940dc98ec219594b1dc857817
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2021-01-12 15:31:34 +0300

    Select2 related classes added.

---
+++ Adverthouse.Utility/Select2/Select2PagedResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adverthouse.Utility.Select2
+{
+    public class Select2PagedResult
+    {
+        public int Total { get; set; }
+        public List<Select2Result> Results { get; set; }
+    }
+}
---
+++ Adverthouse.Utility/Select2/Select2Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adverthouse.Utility.Select2
+{
+    public class Select2Result
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+    }
+}
