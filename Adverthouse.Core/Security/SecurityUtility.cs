939b1f0c8ce56d913ffe50862a973392df4019ea
Yunus OZTURK <yunusozturk@gmail.com>
2021-01-13 15:55:26 +0300

    .

--- Adverthouse.Core/Security/SecurityUtility.cs
+++ Adverthouse.Core/Security/SecurityUtility.cs
@@ -1,24 +1,63 @@
 ﻿using Microsoft.AspNetCore.DataProtection;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Adverthouse.Core.Security
 {
     public static class SecurityUtility
     {
-        public static string Encrypt(string key, string data, string applicationName = "App")
+        public static string Encrypt(string key, string plainText)
         {
-            var dataProtectionProvider = DataProtectionProvider.Create(applicationName);
-            var protector = dataProtectionProvider.CreateProtector(key);
-            return protector.Protect(data);
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
         }
 
-        public static string Decrypt(string key, string data, string applicationName = "App")
+        public static string Decrypt(string key, string cipherText)
         {
-            var dataProtectionProvider = DataProtectionProvider.Create(applicationName);
-            var protector = dataProtectionProvider.CreateProtector(key);
-            return protector.Unprotect(data);
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
         }
     }
 }
