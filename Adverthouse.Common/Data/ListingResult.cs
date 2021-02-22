e8e8508338f71de19fa2f479810b061b5319f58a
Yunus OZTURK <yunusozturk@gmail.com>
2021-02-22 14:57:32 +0300

    .

--- Adverthouse.Common/Data/ListingResult.cs
+++ Adverthouse.Common/Data/ListingResult.cs
@@ -7,10 +7,9 @@ namespace Adverthouse.Common.Data
     public class ListingResult<T, PSF>
     {
         public IEnumerable<T> Data { get; set; }
-        public PSF PagingInfo { get; set; }
-        public string CurrentPage { get; set; }                
-        public ListingResult() => CurrentPage = ""; 
-        public ListingResult(PSF pagingInfo, IEnumerable<T> data) :base()
+        public PSF PagingInfo { get; set; }              
+        public ListingResult() { }
+        public ListingResult(PSF pagingInfo, IEnumerable<T> data) 
         {
             PagingInfo = pagingInfo;
             Data = data;
