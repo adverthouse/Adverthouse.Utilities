b256fee5b8f5afb7ef4e734c3a10f7767522632f
yunus.ozturk <yunus.ozturk@boytas.com.tr>
2021-12-27 09:44:31 +0300

    Status info added for json result

---
+++ Adverthouse.Common/Data/StatusInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adverthouse.Common.Data
+{
+    public class StatusInfo
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public int ID { get; set; }
+        public object Data { get; set; }
+
+        public StatusInfo()
+        {
+            StatusCode = (Int32)StatusInfoCodes.Error;
+        }
+
+        public StatusInfo(StatusInfoCodes statusCode)
+        {
+            StatusCode = (Int32)statusCode;
+        }
+    }
+}
---
+++ Adverthouse.Common/Data/StatusInfoCodes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adverthouse.Common.Data
+{ 
+    public enum StatusInfoCodes
+    {
+        Error = 5000,
+        Ok = 2000,
+        Authenticated = 1000,
+        MistakePassword = 1001,
+        UserNotFound = 1002,
+        IncorrectPassword = 1003,
+        InvalidApiKey = 1004,
+        ApiKeyExpired = 1005,
+        NotFound = 1006,
+        NotAcceptedTerms = 2000,
+        UploadError = 3000,
+        CaptchaError = 3001,
+        AlreadyExists = 20001
+    }
+}
