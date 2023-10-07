077daba224b8c49a438d969f87b417c95c3e3d90
yunusozturk@msn.com <yunusozturk@msn.com>
2023-10-07 21:42:52 +0300

    .

--- Adverthouse.Common/Data/StatusInfo.cs
+++ Adverthouse.Common/Data/StatusInfo.cs
@@ -5,16 +5,10 @@ namespace Adverthouse.Common.Data
 {
     public class StatusInfo<TData,TId>
     {
-        public int StatusCode { get; set; }
+        public int StatusCode { get; set; } = StatusCodes.Status400BadRequest;
         public string StatusMessage { get; set; } = String.Empty;
         public TId ID { get; set; }
         public TData Data { get; set; }
-
-        public StatusInfo()
-        {
-            StatusCode = StatusCodes.Status400BadRequest;
-        }
-
         public StatusInfo(int statusCode)
         {
             StatusCode = statusCode;
