fb716067132fa8cd8fdbeb7d7d1d65eb79f0b5d7
yunusozturk@msn.com <yunusozturk@msn.com>
2023-10-31 20:54:36 +0300

    .

--- Adverthouse.Core/Configuration/PayPalConfig.cs
+++ Adverthouse.Core/Configuration/PayPalConfig.cs
@@ -3,7 +3,7 @@
     public class PayPalConfig : IConfig
     {
         public string ClientId { get; set; } = "";        
-        public string ClientScreet { get; set; } = ""; 
+        public string ClientSecret { get; set; } = ""; 
         public string Mode { get; set; } = "sandbox"; 
     }
 }
